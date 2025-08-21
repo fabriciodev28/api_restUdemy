22618fa91df2d950584cfe45b804ecb0673f635a
fabriciodev28 <fabriciodev28@gmail.com>
2025-08-21 12:05:29 -0300

    criado rota dee alunos

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno'
-
 class HomeController {
     async index(req, res){
-        const novoAluno = await Aluno.create({
-            nome: 'Joao',
-            sobrenome: 'Silva',
-            email: 'joao@gmail.com',
-            idade: 23,
-            peso: 60,
-            altura: 1.65
-        })
-        res.json(novoAluno)
+        res.json('Index')
     }
 }
 
